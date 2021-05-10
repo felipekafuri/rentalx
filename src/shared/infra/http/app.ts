eac2f6b780361fd24a748a68b39b3407aabdaea3
felipekafuri <felipekafuri@discente.ufg.br>
2021-05-10 10:30:28 -0300

    add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -13,6 +13,8 @@ import { routes } from '@shared/infra/http/routes'
 import swaggerFile from '../../../swagger.json'
 import upload from '@config/upload'
 
+import cors from 'cors'
+
 createConnection()
 const app = express()
 app.use(express.json())
@@ -21,6 +23,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`))
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`))
+app.use(cors())
 app.use(routes)
 
 app.use(
